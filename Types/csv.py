62a013204ab102b2340a93d79258c5bebe60a5c7
dr-doc-phd <74687769+dr-doc-phd@users.noreply.github.com>
2022-09-15 13:08:23 -0400

    removed redundant csv fucntion in main, tweaked imports for csv and main to resolve import errors.

--- Types/csv.py
+++ Types/csv.py
@@ -1,4 +1,4 @@
-from row import row
+from Types.row import Row
 
 def improved_eval(n):
     try:
@@ -10,13 +10,13 @@ def improved_eval(n):
 # and stores them in the relevant location in a 2d array
 
 
-def csv(filename, function):
+def csv(filename):
     file = open(filename, "r")
     file1 = file.readlines()
     input = [l.split(',') for l in '\n'.join(file1).split('\n\n')]
     output = [list(map(improved_eval, i)) for i in input]
     rows = []
     for j in output:
-        a = row(j)
+        a = Row(j)
         rows.append(a)
     return rows
--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 # this function handles error catching with the eval function
 from eg.eg import *
-def improved_eval(n):
-    try:
-        output = eval(n)
-    except (NameError, SyntaxError):
-        output = eval(repr(n))
-    return output
-# this function reads in a csv file, converts the strings to a relevant data type,
-# and stores them in the relevant location in a 2d array
-
-
-def csv(filename):
-    file = open(filename, "r")
-    file1 = file.readlines()
-    input = [l.split(',') for l in '\n'.join(file1).split('\n\n')]
-    output = [list(map(improved_eval, i)) for i in input]
-    return output
-
+from Types.csv import csv
 
 if __name__ == '__main__':
     sample = csv('Files/auto93.csv')
